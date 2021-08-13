c10f3e379f81babb88c34fafb1d3b4dc1b4ee746
Kim-tang2 <rlaxogns4504@gmail.com>
2021-08-14 08:03:10 +0900

    Fix: CORS 설정 allow '*' 매우 위험함. 나중에 꼭 수정해야함.

--- back/app.js
+++ back/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 const db = require('./models');
@@ -10,6 +11,11 @@ db.sequelize.sync()
     })
     .catch(console.error)
 
+app.use(cors({
+    // 보안상의 이유로 추후 바꿔줘야함.
+    origin: '*',
+    credential: false,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
