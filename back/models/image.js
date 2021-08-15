50419830f159dd01b596d9932973f8a5461a4323
Kim-tang2 <rlaxogns4504@gmail.com>
2021-08-15 20:41:15 +0900

    Refactoring: Model

--- back/models/image.js
+++ back/models/image.js
@@ -1,14 +1,22 @@
-module.exports = (sequelize, DataTypes) => {
-    const Image = sequelize.define('Image', {
-        src: {
-            type: DataTypes.STRING(200),
-        },
-    }, {
-        charset: 'utf8',
-        collate: 'utf8_general_ci'
-    });
-    Image.association = (db) => {
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
+
+module.exports = class Image extends Model {
+    static init(sequelize) {
+        return super.init({
+            src: {
+                type: DataTypes.STRING(200),
+                allowNull: false,
+            },
+        }, {
+            modelName: 'Image',
+            tableName: 'images',
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+            sequelize,
+        });
+    }
+    static associate(db) {
         db.Image.belongsTo(db.Post);
-    };
-    return Image;
-}
+    }
+};
