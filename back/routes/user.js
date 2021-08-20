b8ab681efe8a97cb2f102da0887b8659f5bf64fa
Kim-tang2 <rlaxogns4504@gmail.com>
2021-08-20 16:53:58 +0900

    feat: 팔로우, 언팔로우 구현

--- back/routes/user.js
+++ back/routes/user.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     try {
-        if(req.user) {
+        if (req.user) {
             const fullUserWithoutPassword = await User.findOne({
                 where: {id: req.user.id},
                 attributes: {
@@ -103,13 +103,43 @@ router.post('/logout', isLoggedIn, (req, res) => {
 });
 
 router.patch('/nickname', isLoggedIn, async (req, res, next) => {
-    try{
+    try {
         await User.update({
-           nickname: req.body.nickname,
+            nickname: req.body.nickname,
         }, {
-            where: { id : req.user.id },
+            where: {id: req.user.id},
         });
-        res.status(200).json({ nickname: req.body.nickname });
+        res.status(200).json({nickname: req.body.nickname});
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.patch('/:userId/follow', isLoggedIn, async (req, res, next) => {
+    try {
+        const user = await User.findOne({ where: {id: req.params.userId}} );
+        if (!user) {
+            res.status(403).send('존재하지 않는 사용자입니다.');
+        }
+
+        await user.addFollowers(req.user.id);
+        res.status(200).json({UserId: parseInt(req.params.userId, 10) });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.delete('/:userId/follow', isLoggedIn, async (req, res, next) => {
+    try {
+        const user = await User.findOne({ where: {id: req.params.userId}} );
+        if (!user) {
+            res.status(403).send('존재하지 않는 사용자입니다.');
+        }
+
+        await user.removeFollowers(req.user.id);
+        res.status(200).json({UserId: parseInt(req.params.userId, 10)});
     } catch (error) {
         console.error(error);
         next(error);
