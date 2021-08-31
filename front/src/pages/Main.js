66913d5b7cd877f12fe132672ed6e06c562c7712
BaekYeonSun <55418359+BaekYeonSun@users.noreply.github.com>
2021-08-31 17:33:36 +0900

    ignore

--- front/src/pages/Main.js
+++ front/src/pages/Main.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Footer from "../components/common/footer";
+import Planet from "../components/main/planet";
 
 const Main = (props) => {
     return (
         <div>
-            행성 메인 페이지
-            <Footer />
+            <Planet />
+            {/*행성 메인 페이지*/}
+            {/*<Footer />*/}
         </div>
     )
 }
