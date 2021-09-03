3c19946d370eb709322f1067e0180a7db9a66965
hyunseo <hansx2079@naver.com>
2021-09-03 23:58:48 +0900

    Create Page 추가

--- front/src/App.js
+++ front/src/App.js
@@ -3,26 +3,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Main from "./pages/Main";
-import Write from "./pages/Write";
-import Create from "./pages/Create";
-import List from "./pages/List";
-import BlackHole from "./pages/BlackHole";
-import Statistics from "./pages/Statistics"
+import Create from './pages/Create'
 
 class App extends Component {
   render() {
     return (
-        <Router>
-          <div>
-            <Route exact path="/main" component={Main} />
-            <Route path="/write" component={Write} />
-            <Route path="/create" component={Create} />
-            <Route path="/list" component={List} />
-            <Route path="/blackHole" component={BlackHole} />
-            <Route path="/statistics" component={Statistics} />
-          </div>
-        </Router>
+      <Create/>
     );
   }
 }
