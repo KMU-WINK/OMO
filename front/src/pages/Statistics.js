a8306fcd11c79c4e400d34ef67803f89ea906232
Sang-Lim <54923245+sanglim00@users.noreply.github.com>
2021-09-03 14:46:36 +0900

    Add files via upload

---
+++ front/src/pages/Statistics.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Header from "../components/common/header";
+import Footer from "../components/common/footer";
+
+const Statistics = (props) => {
+
+    return (
+        <div>
+            <Header state={"Back"} title={"통계 페이지"}/>
+                통계 페이지 테스트
+            <Footer />
+        </div>
+    )
+}
+
+export default Statistics;
