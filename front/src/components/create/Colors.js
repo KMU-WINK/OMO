ba3f4f0a5ebcd558ad1c6572fda92689bc0fff4a
hyunseo <hansx2079@naver.com>
2021-09-07 06:35:10 +0900

    Planet, Color Load 최적화
    Color.js, planets.js

--- front/src/components/create/Colors.js
+++ front/src/components/create/Colors.js
@@ -1,19 +1,8 @@
-const color0 = require('../../images/common/planets/color0.png').default;
-const color1 = require('../../images/common/planets/color1.png').default;
-const color2 = require('../../images/common/planets/color2.png').default;
-const color3 = require('../../images/common/planets/color3.png').default;
-const color4 = require('../../images/common/planets/color4.png').default;
-const color5 = require('../../images/common/planets/color5.png').default;
-
-const loadColor = (colorId) =>{
-    return eval("color" + colorId);
-}
-
 let colors = []
 const colorLength = 6;
 for(let i=0;i<colorLength;i++){
     colors = colors.concat({
-        color:loadColor(i),
+        color:require('../../images/common/planets/color' + i + '.png').default,
         id:i,
     });
 }
--- front/src/components/create/planets.js
+++ front/src/components/create/planets.js
@@ -20,39 +20,22 @@ const planets = [
     addPlanet(planet5, false),
 ]
 
-const planet0_0 = require('../../images/common/planets/planet0_0.png').default;
-const planet0_1 = require('../../images/common/planets/planet0_1.png').default;
-const planet0_2 = require('../../images/common/planets/planet0_2.png').default;
-const planet0_3 = require('../../images/common/planets/planet0_3.png').default;
-const planet0_4 = require('../../images/common/planets/planet0_4.png').default;
-const planet0_5 = require('../../images/common/planets/planet0_5.png').default;
-const planet1_0 = require('../../images/common/planets/planet0_0.png').default;
-const planet1_1 = require('../../images/common/planets/planet1_1.png').default;
-const planet1_2 = require('../../images/common/planets/planet1_2.png').default;
-const planet1_3 = require('../../images/common/planets/planet1_3.png').default;
-const planet1_4 = require('../../images/common/planets/planet1_4.png').default;
-const planet1_5 = require('../../images/common/planets/planet1_5.png').default;
-const planet2_0 = require('../../images/common/planets/planet2_0.png').default;
-const planet2_1 = require('../../images/common/planets/planet2_1.png').default;
-const planet2_2 = require('../../images/common/planets/planet2_2.png').default;
-const planet2_3 = require('../../images/common/planets/planet2_3.png').default;
-const planet2_4 = require('../../images/common/planets/planet2_4.png').default;
-const planet2_5 = require('../../images/common/planets/planet2_5.png').default;
-const planet3_0 = require('../../images/common/planets/planet3_0.png').default;
-const planet3_1 = require('../../images/common/planets/planet3_1.png').default;
-const planet3_2 = require('../../images/common/planets/planet3_2.png').default;
-const planet3_3 = require('../../images/common/planets/planet3_3.png').default;
-const planet3_4 = require('../../images/common/planets/planet3_4.png').default;
-const planet3_5 = require('../../images/common/planets/planet3_5.png').default;
-const planet4_0 = require('../../images/common/planets/planet4_0.png').default;
-const planet4_1 = require('../../images/common/planets/planet4_1.png').default;
-const planet4_2 = require('../../images/common/planets/planet4_2.png').default;
-const planet4_3 = require('../../images/common/planets/planet4_3.png').default;
-const planet4_4 = require('../../images/common/planets/planet4_4.png').default;
-const planet4_5 = require('../../images/common/planets/planet4_5.png').default;
+const planetCount = 5;
+const colorCount = 6;
+const planetImages = new Array(planetCount);
+for(let i=0;i<colorCount;i++){
+    planetImages[i] = new Array(colorCount);
+}
+
+for(let i=0;i<planetCount;i++){
+    for(let j=0;j<colorCount;j++){
+        planetImages[i][j] = require('../../images/common/planets/planet' + i + '_' + j + '.png').default;
+    }
+}
 
 const loadPlanet = (planetId, colorId) =>{
-    return eval("planet" + planetId + "_" + colorId);
+    console.log(planetImages);
+    return planetImages[planetId][colorId];
 }
 
 export {planets, loadPlanet};
