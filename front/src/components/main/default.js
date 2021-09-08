a8d922bdc7658fb2dce9aae1ee03c581ece21c15
Sanglim00 <sanglim@kookmin.ac.kr>
2021-09-09 01:29:53 +0900

    통계페이지 임시 이미지 추가 메인 버튼 default페이지에도 추가

--- front/src/components/main/default.js
+++ front/src/components/main/default.js
@@ -3,6 +3,8 @@ import {useHistory} from "react-router-dom";
 
 import blurPlanet from '../../images/main/main_blurPlanet.png';
 import bbuyo from '../../images/main/main_bbuyo.png';
+import moreDetailButton from '../../images/main/main_moreDetailButton.png';
+import statisticsButton from '../../images/main/main_statisticsButton.png';
 
 const textCSS = {
     textAlign: 'center',
@@ -40,9 +42,40 @@ const txtCSS = {
     margin: 0,
     paddingBottom:'6px',
 }
+const moreDetailButtonImg = {
+    marginTop: '170px',
+    marginLeft: '284px',
+    width: '25px',
+    height: '25px',
+    position: 'absolute',
+    display: 'none',
+    cursor: 'pointer',
+}
+const statisticsButtonImg = {
+    marginTop: '190px',
+    marginLeft: '245px',
+    width: '25px',
+    height: '25px',
+    position: 'absolute',
+    display: 'none',
+    cursor: 'pointer',
+}
 
 const Default = (props) => {
     const history = useHistory();
+
+    const [checkShowButtons, setCheckShowButtons] = useState(false);
+    const showButtons = () => {
+        if(checkShowButtons === false){
+            document.getElementById("showMoreDetailButtons").style.display = 'block';
+            document.getElementById("showStatisticsButtons").style.display = 'block';
+        }
+        else{
+            document.getElementById("showMoreDetailButtons").style.display = 'none';
+            document.getElementById("showStatisticsButtons").style.display = 'none';
+        }
+        setCheckShowButtons(!checkShowButtons)
+    }
 
     return (
         <div>
@@ -50,9 +83,11 @@ const Default = (props) => {
                 아래 + 버튼을 눌러<br/>새 감정을 기록해주세요
             </div>
             <div style={{marginTop: '40px'}}>
+                <img src={moreDetailButton} id={"showMoreDetailButtons"} style={moreDetailButtonImg} onClick={() => history.push('/moreDetail')}/>
+                <img src={statisticsButton} id={"showStatisticsButtons"} style={statisticsButtonImg} onClick={() => history.push('/statistics')}/>
                 <div style={blurPlanetImg} >
                     <img src={blurPlanet} style={blurPlanetCSS}/>
-                    <div style={bbuyoCSS} onClick={() => history.push('/statistics')}>
+                    <div style={bbuyoCSS} onClick={showButtons}>
                         <p style={txtCSS}>뿌요</p>
                         <img src={bbuyo} style={bbuyoStyle} />
                     </div>
