16b1fde0d34a80941590001273aa13fc5d7fbc6d
Sanglim00 <sanglim@kookmin.ac.kr>
2021-09-08 22:16:54 +0900

    main 페이지 버튼 추가 더보기 페이지 제작 시작

---
+++ front/src/pages/MoreDetail.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, Component} from 'react';
+
+function MoreDetail(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default MoreDetail;
